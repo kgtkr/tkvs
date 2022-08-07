acf16f5eded3bbc8d82fe725cf615edc471e225b
kgtkr <contact@kgtkr.net>
2022-08-08 05:10:59 +0900

    crossコンパイルをnixに乗っかった

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      {
+        devShell =
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+            };
+          in
+          with pkgs; mkShell {
+            nativeBuildInputs = [
+              protobuf
+              wget
+              qemu
+              gnumake
+              sshpass
+            ];
+          };
+        devShells.aarch64-linux-cross =
+          let
+            pkgs = import nixpkgs {
+              system = if system == "aarch64-darwin" then "x86_64-darwin" else system;
+              crossSystem = "aarch64-linux";
+            };
+          in
+          with pkgs; mkShell {
+            nativeBuildInputs = [
+              pkg-config
+            ];
+            CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
+          };
+      }
+    );
+}
