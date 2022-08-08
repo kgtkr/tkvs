0b45e9a0a16e16d2f6fc771df216c8b3493bcdac
kgtkr <contact@kgtkr.net>
2022-08-09 04:13:15 +0900

    arch上で動かすようにした

--- flake.nix
+++ flake.nix
@@ -21,17 +21,17 @@
               gnumake
               sshpass
               patchelf
-              (callPackage ./cloud-localds.nix {})
             ];
             buildInputs = lib.optionals stdenv.isDarwin [
               libiconv
             ];
           };
-        devShells.aarch64-linux-cross =
+        devShells.x86_64-linux-cross =
           let
             pkgs = (import nixpkgs {
               system = if system == "aarch64-darwin" then "x86_64-darwin" else system;
-            }).pkgsCross.aarch64-multiplatform-musl.pkgsStatic;
+              crossSystem = "x86_64-linux";
+            }).pkgsStatic;
           in
           with pkgs; mkShell {
             nativeBuildInputs = [
@@ -40,7 +40,7 @@
             buildInputs = [
               libiconv
             ];
-            CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER = "${stdenv.cc.targetPrefix}cc";
+            CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = "${stdenv.cc.targetPrefix}cc";
           };
       }
     );
